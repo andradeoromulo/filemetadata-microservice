97d9c792c05f562d7ea5bdef819bbb04e0bb88b7
Rômulo Andrade <56308434+andradeoromulo@users.noreply.github.com>
2020-12-17 11:15:31 -0300

    Implement endpoint to upload file

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(`${__dirname}/views/index.html`);
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  
+  const out = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
 
+  res.json(out);
 
+});
+
+app.use(function(req, res) {
+  res.status(404).sendFile(`${__dirname}/views/404.html`);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
